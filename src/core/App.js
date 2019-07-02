3ca1b7b6532afd410259624ce1d288ecace64f6b
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-07-02 14:16:57 +1200

    Renamed main folder to core

---
+++ src/core/App.js
@@ -0,0 +1,14 @@
+import { hot } from 'react-hot-loader';
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      Header
+      Content
+    </div>
+  );
+};
+
+export default hot(module)(App);
